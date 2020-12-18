af685723119ef700a4bf08322be67bd6b1ae5497
Mile <milevuletic@national.shitposting.agency>
2020-12-18 20:12:11 +0100

    Extended play button, shade background on hover; Add AHK reset to log

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = require('socket.io-client');
 function listItems(items) {
     return items?.map(i => `<li class="list-group-item text-muted text-monospace">
                                 ${i}
-                                <button type="button" class="close" aria-label="Delete" data-id="${i}" onclick="(function n(){v = new Event('playSound'); v.whom=this; document.dispatchEvent(v)}).call(this)">
+                                <button type="button" class="close stretched-link" aria-label="Delete" data-id="${i}" onclick="(function n(){v = new Event('playSound'); v.whom=this; document.dispatchEvent(v)}).call(this)">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                       <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                                     </svg>
@@ -160,6 +160,7 @@ $(
     () => {
         $(document).on("resetAhk", () => {
             socket.emit("reset", {target: $('#single-user-select').val()});
+            addToLog(`Reset ${target}'s AHK script`);
         });
     }
 )
